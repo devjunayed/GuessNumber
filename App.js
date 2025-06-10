56a9d6c308399575ccb7b075f9d75d8dbad9588a
Md Junayed <devjunayed@gmail.com>
2025-06-10 16:42:30 +0600

    start screen background changed

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import StartGameScreen from './screens/StartGameScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, ImageBackground } from "react-native";
+import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
   return (
-    <View>
-      <StartGameScreen />
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+      <ImageBackground
+        resizeMode="cover"
+        imageStyle={styles.backgroundImage}
+        style={styles.rootScreen}
+        source={require("./assets/images/dices.jpg")}
+      >
+        <StartGameScreen />
+        <StatusBar style="light" />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
-
+const styles = new StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15
+  }
+});
